3136656c61cf2cd30aedcd18e2c3c25fa97eed44
Brad Slattman <slattman@gmail.com>
2025-01-02 13:46:31 -0500

    updates

--- dist/index.js
+++ dist/index.js
@@ -78232,7 +78232,7 @@ const convert_group_to_h2_markdown = (group_key, group_value) => {
       .addConfig('user.name', 'Brad Slattman')
       .addConfig('user.email', 'slattman@gmail.com')
       .commit('updates')
-      .push(['-u', 'origin', 'main']);
+      .push(['-u', '--force', 'origin', 'main']);
 
 })();
 module.exports = __webpack_exports__;
