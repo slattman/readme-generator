4a20a6d345d4d411ab5f1b843330aef2fca97f77
Brad Slattman <slattman@gmail.com>
2025-01-02 05:05:01 -0500

    updates

--- index.js
+++ index.js
@@ -36,31 +36,30 @@ const sorted_map_keys = (map) => {
 
 const convert_maps_to_toc_markdown = (map) => {
   let sorted_keys = sorted_map_keys(map);
-  let toc = ['## Contents\n'];
+  let toc = [''];
   for (const key of sorted_keys) {
     const group_name = `${key}`.replace(/ /g, '-');
-    const group = `- [${key}](#${group_name})`;
+    const group = `[${key}](#${group_name})`;
     toc.push(group);
   }
-  return toc.join('\n');
+  return toc.join(' ✨ ');
 };
 
 const convert_group_to_h2_markdown = (group_key, group_value) => {
   const group_name = `${group_key}`.replace(/ /g, '-');
-  let toc = [`## ${group_name}\n`];
+  let toc = [`## ✨ ${group_name}\n`];
   for (const repo of group_value) {
-    const group = `- [${repo.name}](${repo.url}) - ${repo.description}`;
+    const group = `- [${repo.name}](${repo.html_url}) - ${repo.description}`;
     toc.push(group);
   }
-  return toc.join(' ');
+  return toc.join('\n');
 }
 
 (async () => {
-  try {
     const owner = core.getInput('owner');
     const myToken = core.getInput('github-token');
     const octokit = simpleOctokit(myToken);
-
+    const topicMap = new Map();
     const repos = await fetch_starred_repos_with_language(octokit, owner);
     for (const repo of repos) {
       if (repo.language) {
@@ -71,28 +70,22 @@ const convert_group_to_h2_markdown = (group_key, group_value) => {
     }
     
     let markdown = [`
-      <div align="center" width='100%'>
-      <p align="center">
-          <a href="https://github.com/slattman?tab=repositories">
-              <img src="https://github-readme-activity-graph.vercel.app/graph?username=slattman&theme=react-dark&hide_border=true&hide_title=false&area=true&custom_title=Total%20contribution%20graph%20in%20all%20repo" width="98%" alt="activity graph">
-          </a>
-      </p>
-      <p align="center">
-        <a href="https://github.com/slattman?tab=stars">
-          <img src="https://github-readme-streak-stats.herokuapp.com?user=slattman&theme=gotham&hide_border=true&date_format=M%20j%5B%2C%20Y%5D"  width="49%" alt="github-readme-streak-stats"/>
-        </a>
-        <a href="https://github.com/slattman?tab=repositories">
-          <img src="https://github-readme-stats-one-bice.vercel.app/api?username=slattman&theme=gotham&show_icons=true&count_private=true&hide_border=true&role=OWNER,ORGANIZATION_MEMBER,COLLABORATOR"  width="49%" alt="github-readme-stats"/>
-        </a>
-      </p>
-      <p align="center">
-      <a href="https://github.com/slattman?tab=stars">
-        <img width="50%" height="250px" src="https://github-readme-stats.vercel.app/api/top-langs/?username=slattman&layout=compact&hide_border=true&title_color=00b3ff&text_color=00b4ff&bg_color=0d1117" /></a>
-        <a href="https://github.com/slattman?tab=achievements">
-          <img src="https://github-profile-trophy.vercel.app/?username=slattman&theme=onestar&no-frame=true&column=4&row=2" width="49%" height="250px" alt="trophy stats"/>
-        </a>
-      </p>
-      \n\n`];
+<div>
+  <a href="https://github.com/slattman?tab=stars">
+    <img width="49%" src="https://github-readme-streak-stats.herokuapp.com?user=slattman&theme=react&hide_border=true&date_format=M%20j%5B%2C%20Y%5D" />
+  </a>
+  <a href="https://github.com/slattman?tab=repositories">
+    <img width="49%" src="https://github-readme-stats-one-bice.vercel.app/api?username=slattman&theme=react&show_icons=true&count_private=true&hide_border=true&role=OWNER,ORGANIZATION_MEMBER,COLLABORATOR" />
+  </a>
+  <a href="https://github.com/slattman?tab=repositories">
+      <img width="57%" src="https://github-readme-activity-graph.vercel.app/graph?username=slattman&theme=react&radius=50&hide_border=true&hide_title=false&area=true&custom_title=Total%20contribution%20graph%20in%20all%20repo" >
+  </a>
+  <a href="https://github.com/slattman?tab=achievements">
+    <img width="40%" src="https://github-profile-trophy.vercel.app/?username=slattman&theme=discord&no-frame=true&row=2&column=4" />
+  </a>
+</div>
+<hr />
+    \n\n`];
 
     const toc = convert_maps_to_toc_markdown(topicMap);
     markdown.push(toc);
@@ -115,7 +108,5 @@ const convert_group_to_h2_markdown = (group_key, group_value) => {
     const result = markdown.join("\n\n");
     fs.writeFileSync('README.md', result);
 
-  } catch (error) {
-    core.setFailed(error.message);
-  }
+  
 })();
