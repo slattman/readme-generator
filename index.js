52f5035f22ddd72be949f307726198e10d5b4a96
Brad Slattman <slattman@gmail.com>
2025-01-02 13:18:24 -0500

    updates

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const core = require('@actions/core');
+const git = require('simple-git');
 const simpleOctokit = require('simple-octokit');
+const wget = require('node-wget');
 const fs = require('fs');
 
 const fetch_starred_repos_with_language = async (octokit, username) => {
@@ -59,54 +61,60 @@ const convert_group_to_h2_markdown = (group_key, group_value) => {
     const owner = core.getInput('owner');
     const token = core.getInput('github-token');
     const octokit = simpleOctokit(token);
-    const topicMap = new Map();
+    const topic = new Map();
     const repos = await fetch_starred_repos_with_language(octokit, owner);
     for (const repo of repos) {
       if (repo.language) {
-        upsert(topicMap, repo.language, repo);
+        upsert(topic, repo.language, repo);
       } else {
-        upsert(topicMap, 'etc', repo);
+        upsert(topic, 'etc', repo);
       }
     }
     
     let markdown = [`
 <div>
-  <a href="https://github.com/slattman?tab=stars">
-    <img width="49%" src="https://github-readme-streak-stats.herokuapp.com?user=slattman&theme=react&hide_border=true&date_format=M%20j%5B%2C%20Y%5D" />
-  </a>
-  <a href="https://github.com/slattman?tab=repositories">
-    <img width="49%" src="https://github-readme-stats-one-bice.vercel.app/api?username=slattman&theme=react&show_icons=true&count_private=true&hide_border=true&role=OWNER,ORGANIZATION_MEMBER,COLLABORATOR" />
-  </a>
-  <a href="https://github.com/slattman?tab=repositories">
-      <img width="57%" src="https://github-readme-activity-graph.vercel.app/graph?username=slattman&theme=react&radius=50&hide_border=true&hide_title=false&area=true&custom_title=Total%20contribution%20graph%20in%20all%20repo" >
-  </a>
-  <a href="https://github.com/slattman?tab=achievements">
-    <img width="40%" src="https://github-profile-trophy.vercel.app/?username=slattman&theme=discord&no-frame=true&row=2&column=4" />
-  </a>
+<img width="49%" src="streak.svg" />
+<img width="49%" src="stats.svg" />
+<img width="57%" src="activity.svg" >
+<img width="40%" src="trophy.svg" />
 </div>
 <hr />
     \n\n`];
 
-    const toc = convert_maps_to_toc_markdown(topicMap);
+    const toc = convert_maps_to_toc_markdown(topic);
     markdown.push(toc);
 
-    const sorted_keys = sorted_map_keys(topicMap);
+    const sorted_keys = sorted_map_keys(topic);
     for (const group_name of sorted_keys) {
       if (group_name == 'etc') { continue; }
-      const group_value = topicMap.get(group_name);
+      const group_value = topic.get(group_name);
       const group = convert_group_to_h2_markdown(group_name, group_value);
       markdown.push(group);
     }
 
-    if (topicMap.has('etc')) {
+    if (topic.has('etc')) {
       const group_name = 'etc';
-      const group_value = topicMap.get(group_name);
+      const group_value = topic.get(group_name);
       const group = convert_group_to_h2_markdown(group_name, group_value);
       markdown.push(group);
     }
 
     const result = markdown.join("\n\n");
-    fs.writeFileSync('../slattman/README.md', result);
+    fs.writeFileSync('./test.md', result);
+    const svgs = [
+      { "dest": "./streak.svg", "url": "https://github-readme-streak-stats.herokuapp.com?user=slattman&theme=react&hide_border=true&date_format=M%20j%5B%2C%20Y%5D" },
+      { "dest": "./stats.svg", "url": "https://github-readme-stats-one-bice.vercel.app/api?username=slattman&theme=react&show_icons=true&count_private=true&hide_border=true&role=OWNER,ORGANIZATION_MEMBER,COLLABORATOR" },
+      { "dest": "./activity.svg", "url": "https://github-readme-activity-graph.vercel.app/graph?username=slattman&theme=react&radius=50&hide_border=true&hide_title=false&area=true&custom_title=Total%20contribution%20graph%20in%20all%20repo" },
+      { "dest": "./trophy.svg", "url": "https://github-profile-trophy.vercel.app/?username=slattman&theme=discord&no-frame=true&row=2&column=4" }
+    ];
+    svgs.every(async (svg) => {
+      await wget({
+          url: svg.url,
+          dest: svg.dest
+      });
+    });
+    await git().add('.')
+      .commit('updates')
+      .push(['-u', 'origin', 'main']);
 
-  
 })();
